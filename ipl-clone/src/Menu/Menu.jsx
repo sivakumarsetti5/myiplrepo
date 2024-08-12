cef46c81a3c5a13d8d123f15ae975a8405687e38
sivakumarsetti <sivakumarsettii@gmail.com>
2024-08-12 21:35:16 +0530

    third commit

--- ipl-clone/src/Menu/Menu.jsx
+++ ipl-clone/src/Menu/Menu.jsx
@@ -10,11 +10,11 @@ export const Menu = () => {
       <li ><Link className={styles.listItem} href='/mi'>MI</Link></li>
       <li ><Link className={styles.listItem} href='/srh'>SRH</Link></li>
       <li ><Link className={styles.listItem} href='/kkr'>KKR</Link></li>
-      <li ><Link className={styles.listItem} href='/dc'>DC</Link></li>
-      <li ><Link className={styles.listItem} href='/rr'>RR</Link></li>
-      <li ><Link className={styles.listItem} href='/lsg'>LSG</Link></li>
-      <li ><Link className={styles.listItem} href='/gt'>GT</Link></li>
-      <li ><Link className={styles.listItem} href='/pbks'>PBKS</Link></li>
+      <li ><Link className={styles.listItem} href='/home'>DC</Link></li>
+      <li ><Link className={styles.listItem} href='/home'>RR</Link></li>
+      <li ><Link className={styles.listItem} href='/home'>LSG</Link></li>
+      <li ><Link className={styles.listItem} href='/home'>GT</Link></li>
+      <li ><Link className={styles.listItem} href='/home'>PBKS</Link></li>
     </ul>
   )
 }
